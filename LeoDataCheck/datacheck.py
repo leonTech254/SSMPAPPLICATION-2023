0256bea46c9331df0072d315acfeb337951abcdb
leontech <martinleontech23@gmail.com>
2023-01-11 19:48:06 +0300

    --databases

--- LeoDataCheck/datacheck.py
+++ LeoDataCheck/datacheck.py
@@ -1,3 +1,13 @@
+import sqlite3
+conn = sqlite3.connect("hashDatabase.db")
+cursor = conn.cursor()
+query="CREATE TABLE  IF NOT EXISTS  Users(username VARCHAR(100),phoneNumber VARCHAR(100),DeviceCode VARCHAR(100),isValidated VARCHAR(100) default 'false')"
+cursor.execute(query)
+# messages
+query="CREATE TABLE  IF NOT EXISTS  Messages(username VARCHAR(100),msg VARCHAR(100),phoneNumber VARCHAR(100),DeviceCode VARCHAR(100), MsgTimeStamp VARCHAR(100),msgCheckSum VARCHAR(100))"
+cursor.execute(query)
+query="CREATE TABLE  IF NOT EXISTS  Chats(username VARCHAR(100),msg VARCHAR(100),DeviceCode VARCHAR(100), MsgTimeStamp VARCHAR(100))"
+cursor.execute(query)
 class Validate:
     def phone(number):
         print(number.isnumeric)
@@ -22,4 +32,50 @@ class Validate:
         else:
             return "username must have atleast 4 characters"
     def CheckCode(code):
-        pass
+        check = f"SELECT * FROM Users WHERE DeviceCode='{code}'"
+        result = cursor.execute(check).fetchall()
+        if not result:
+            return "invalid code"
+        else:
+            return "success"
+        
+        
+class ManageMessages:
+    def Messages(mycode,chatcode):
+        check = f"SELECT * FROM Messages WHERE DeviceCode='{chatcode}'"
+        messages = cursor.execute(check).fetchall()
+        MessagesList=[]
+        if not messages:
+            return "Empty"
+        else:
+            for message in messages:
+                messageDict={}
+                messageDict['deviceID']=message.DeviceCode
+                messageDict['username']=message.username    
+                messageDict['time']=message.MsgTimeStamp
+                messageDict['checksum']=message.msgCheckSum
+                messageDict['msg']=message.msg
+                if message.DeviceCode==mycode:
+                    messageDict['user']='me'
+                else:
+                    messageDict['user']='friend'
+                MessagesList.append(messageDict)
+            return MessagesList
+    def Chats(mycode,chatcode):
+        check = f"SELECT * FROM Chats"
+        messages = cursor.execute(check).fetchall()
+        ChatList=[]
+        if not messages:
+            return "Empty"
+        else:
+            for message in messages:
+                ChatDict={}
+                ChatDict['deviceID']=message.DeviceCode
+                ChatDict['username']=message.username    
+                ChatDict['time']=message.MsgTimeStamp
+                ChatDict['msg']=message.msg
+                ChatList.append(ChatDict)
+            return ChatList
+        
+            
+        
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager,Screen,FadeTransition
+from kivymd.uix.tab import MDTabs,MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.list import MDList,TwoLineAvatarListItem
+from kivy.properties import ObjectProperty,StringProperty
 # data processing 
 from LeoDataCheck.datacheck import Validate
 class WindowManager(ScreenManager):
@@ -13,12 +17,29 @@ class IntoScreen(Screen):
     pass
 class ValidateScreen(Screen):
     pass
+class ChatScreen(Screen):
+    pass
+
+
+class ChatTab(MDFloatLayout,MDTabsBase):
+    pass
+class Status(MDFloatLayout,MDTabsBase):
+    pass
+class Calls(MDFloatLayout,MDTabsBase):
+    pass
+
+class ChatItem(TwoLineAvatarListItem):
+    image=StringProperty()
+    name=StringProperty()
+    last_msg=StringProperty()
+    
 
 
 # file loading
 Builder.load_file("./Screens/Register.kv")
 Builder.load_file("./Screens/ValidateScreen.kv")
-
+Builder.load_file("./Screens/ChatScreen.kv")
+Builder.load_file("./widgets/ChatsLists.kv")
 
 class MainApp(MDApp):
     def build(self):
@@ -26,13 +47,14 @@ class MainApp(MDApp):
         self.theme_cls.primary_palette="Cyan"
         # self.theme_cls.material_style = "M3"
         self.wm=ScreenManager(transition=FadeTransition())
-        screens=[IntoScreen(name="intoScreen"),RegisterScreen(name="registerScreen"),ValidateScreen(name='ValidateScreen')]
+        screens=[IntoScreen(name="intoScreen"),RegisterScreen(name="registerScreen"),ValidateScreen(name='ValidateScreen'),ChatScreen(name="chatScreen")]
         for screen in screens:
             self.wm.add_widget(screen)
         return self.wm
+    def on_start(self):
+        self.wm.current="chatScreen"
     def ManageScreens(self,screen):
         self.wm.current=screen
-        
     def RegisterFunc(self,phone,username):
         phoneResponse=Validate.phone(phone)
         if phoneResponse=="success":
@@ -46,7 +68,11 @@ class MainApp(MDApp):
     def CheckCode(self,code):
         codeResponse=Validate.CheckCode(code=compile)
         if codeResponse=="success":
+            # all user to move next screen
+            self.ManageScreens("chatScreen")
             pass
+        else:
+            self.wm.get_screen(self.wm.current).ids.response.text=codeResponse
         
         
 
